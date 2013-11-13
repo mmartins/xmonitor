a1b4b54d37dc5f5cc2feb5ad84c2f88e625d3e40
Marcelo Martins <martins@cs.brown.edu>
2013-11-13 05:34:31 -0500

    Export event identifiers

---
+++ src/edu/brown/cs/systems/xmonitor/Events.java
@@ -0,0 +1,45 @@
+package edu.brown.cs.systems.xmonitor;
+
+/**
+ * User: martins
+ * Date: 11/12/13
+ * Time: 7:09 PM
+ */
+public class Events {
+    static final int START_WAKELOCK = 1;
+    static final int STOP_WAKELOCK = 2;
+    static final int START_SENSOR = 3;
+    static final int STOP_SENSOR = 4;
+    static final int START_GPS = 5;
+    static final int STOP_GPS = 6;
+    static final int SCREEN_BRIGHTNESS = 7;
+    static final int START_MEDIA = 8;
+    static final int STOP_MEDIA = 9;
+    static final int VIDEO_SIZE = 10;
+    static final int SYSTEM_MEDIA_CALL = 11;
+    static final int SCREEN_ON = 12;
+    static final int SCREEN_OFF = 13;
+    static final int INPUT_EVENT = 14;
+    static final int USER_ACTIVITY = 15;
+    static final int PHONE_ON = 16;
+    static final int PHONE_OFF = 17;
+    static final int PHONE_DATA_CONNECTION_STATE = 18;
+    static final int WIFI_ON = 19;
+    static final int WIFI_OFF = 20;
+    static final int WIFI_RUNNING = 21;
+    static final int WIFI_STOPPED = 22;
+    static final int BLUETOOTH_ON = 23;
+    static final int BLUETOOTH_OFF = 24;
+    static final int FULL_WIFILOCK_ACQUIRED = 25;
+    static final int FULL_WIFILOCK_RELEASED = 26;
+    static final int SCAN_WIFILOCK_ACQUIRED = 27;
+    static final int SCAN_WIFILOCK_RELEASED = 28;
+    static final int WIFI_MULTICAST_ENABLED = 29;
+    static final int WIFI_MULTICAST_DISABLED = 30;
+    static final int ON_BATTERY = 31;
+    static final int RECORD_CURRENT_LEVEL = 32;
+    static final int VIDEO_ON = 33;
+    static final int VIDEO_OFF = 34;
+    static final int AUDIO_ON = 35;
+    static final int AUDIO_OFF = 36;
+}
